86d0a84d412423cff5542716295a365dd265fa38
Ali Raza <137720587+AliRazaAnsari2564@users.noreply.github.com>
2024-07-04 19:14:56 +0500

    Update page.jsx

--- app/(back-office)/dashboard/home/getting-started/page.jsx
+++ app/(back-office)/dashboard/home/getting-started/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function GettingStarted() {
   return (
     <div>
-      <h2>we will Get start from here</h2>
+      <h2>we will Get start from here. are you sure?</h2>
     </div>
   );
 }
