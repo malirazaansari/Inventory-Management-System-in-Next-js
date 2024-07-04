4c5906d2b8787f9007237e1147a3a0d6719b0e2e
Ali Raza <137720587+AliRazaAnsari2564@users.noreply.github.com>
2024-07-04 19:15:38 +0500

    Update page.jsx

--- app/(back-office)/dashboard/home/updates/page.jsx
+++ app/(back-office)/dashboard/home/updates/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Updates() {
   return (
     <div>
-      <h2 className="text-ornage-600">Updates are not Done yet</h2>
+      <h2 className="text-ornage-600">Updates are good or not?</h2>
     </div>
   );
 }
