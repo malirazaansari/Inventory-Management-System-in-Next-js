0e5c6f5101737a2c43456e6047c1374e3f7834ba
Ali Raza <137720587+AliRazaAnsari2564@users.noreply.github.com>
2024-07-07 17:21:01 +0500

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -27,7 +27,7 @@ export default function Login() {
             src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
             alt="logo"
           />
-          Inventory System
+          Inventory management System
         </a>
         <div className="w-full bg-white rounded-lg shadow-2xl dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
